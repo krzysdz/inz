32ddeb2026d5d2b2bb2ea5e5f0b57517cc3cec61
krzysdz <krzysdz@users.noreply.github.com>
2023-01-16 20:55:55 +0100

    Improve theme switcher accessibility, show check
    This commit mirrors twbs/bootstrap#37780 with ~~a small fix~~ a fix from twbs/bootstrap#37836 and adds the missing checkmark icon to the theme dropdown.

--- static/js/theme_toggle.js
+++ static/js/theme_toggle.js
@@ -27,17 +27,27 @@
 
 	setTheme(getPreferredTheme());
 
-	const showActiveTheme = (theme) => {
+	const showActiveTheme = (theme, focus = false) => {
+		const themeSwitcher = document.querySelector("#theme-btn");
+		const themeSwitcherText = document.querySelector("#theme-toggle-text");
 		const activeThemeIcon = document.querySelector(".theme-icon-active use");
 		const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
 		const svgOfActiveBtn = btnToActive.querySelector("svg use").getAttribute("href");
 
 		document.querySelectorAll("[data-bs-theme-value]").forEach((element) => {
 			element.classList.remove("active");
+			element.setAttribute("aria-pressed", "false");
 		});
 
 		btnToActive.classList.add("active");
+		btnToActive.setAttribute("aria-pressed", "true");
 		activeThemeIcon.setAttribute("href", svgOfActiveBtn);
+		const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+		themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+
+		if (focus) {
+			themeSwitcher.focus();
+		}
 	};
 
 	window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
@@ -55,7 +65,7 @@
 				const theme = toggle.getAttribute("data-bs-theme-value");
 				localStorage.setItem("theme", theme);
 				setTheme(theme);
-				showActiveTheme(theme);
+				showActiveTheme(theme, true);
 			});
 		});
 	});
